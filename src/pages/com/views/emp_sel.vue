75b831f0eab792d17cd3f6ec2cebcc36a45aa1f8
renren1993 <1094437360@qq.com>
2018-11-15 17:11:23 +0800

    1

--- src/pages/com/views/emp_sel.vue
+++ src/pages/com/views/emp_sel.vue
@@ -88,5 +88,12 @@ export default {
 <style scoped>
 .r-search input.mui-input-clear{background: #fff;  text-align: left; padding: 0 65px 0 15px; border: 5px solid #f8f8f9; border-radius: 0; height: 35px; margin-bottom: 0;}
 .r-search input.mui-pull-right{ background: #f8f8f9; color: #58cffa; position: absolute; top: 0; height: 37px; right: 0;}
+input[type='submit']:enabled:active, input[type='submit'].mui-active:enabled,
+.mui-btn-primary:enabled:active,
+.mui-btn-primary.mui-active:enabled, .mui-btn-blue:enabled:active, .mui-btn-blue.mui-active:enabled
+{
+    border: none;
+    background-color: #f8f8f9;
+}
 </style>
 
